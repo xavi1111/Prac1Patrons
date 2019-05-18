336ac2101332eb7a7402c7c5ac6ec2721ff666ff
xavier <xavierllubes11@gmail.com>
2019-05-18 20:09:43 +0200

    Organització projecte

--- src/implementations/ImplementationD1.java
+++ src/implementations/ImplementationD1.java
@@ -7,4 +7,13 @@ public class ImplementationD1 implements InterfaceD {
     public ImplementationD1(int i){
         this.i = i;
     }
+
+    @Override
+    public boolean equals(Object o){
+        if(o instanceof ImplementationD1){
+            return ((ImplementationD1)o).i == this.i;
+        }
+        return false;
+
+    }
 }
