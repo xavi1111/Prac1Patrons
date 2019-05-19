e2760d5fb7f159fcbba1d848c04a2bf59422b518
xavier <xavierllubes11@gmail.com>
2019-05-19 19:26:19 +0200

    CachedServiceLocator V2

--- src/servicelocator2/CachedServiceLocator.java
+++ src/servicelocator2/CachedServiceLocator.java
@@ -4,19 +4,39 @@ import exceptions.LocatorError;
 import servicelocator2.interfaces.Factory;
 import servicelocator2.interfaces.ServiceLocator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CachedServiceLocator implements ServiceLocator {
+    private Map<Object, Factory> serviceMap = new HashMap<>();
+    private Map<Object, Object> constantMap = new HashMap<>();
+
     @Override
     public <T> void setService(Class<T> klass, Factory<T> factory) throws LocatorError {
-
+        if (!serviceMap.containsKey(klass))
+            serviceMap.put(klass, factory);
+        else
+            throw new LocatorError();
     }
 
     @Override
     public <T> void setConstant(Class<T> klass, T value) throws LocatorError {
-
+        if (!constantMap.containsKey(klass))
+            constantMap.put(klass, value);
+        else
+            throw new LocatorError();
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <T> T getObject(Class<T> klass) throws LocatorError {
-        return null;
+        if (serviceMap.containsKey(klass)) {
+            setConstant(klass, (T) serviceMap.get(klass).create(this));
+            serviceMap.remove(klass);
+            return (T) constantMap.get(klass);
+        } else if (constantMap.containsKey(klass))
+            return (T) constantMap.get(klass);
+        else
+            throw new LocatorError();
     }
 }
--- src/simpleservicelocatortest/SimpleServiceLocator2Test.java
+++ src/simpleservicelocatortest/SimpleServiceLocator2Test.java
@@ -1,6 +1,13 @@
 package simpleservicelocatortest;
 
 import exceptions.LocatorError;
+import implementations.ImplementationA1;
+import implementations.ImplementationB1;
+import implementations.ImplementationD1;
+import interfaces.InterfaceA;
+import interfaces.InterfaceB;
+import interfaces.InterfaceC;
+import interfaces.InterfaceD;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import servicelocator2.SimpleServiceLocator;
@@ -8,6 +15,7 @@ import servicelocator2.factories.FactoryA1;
 import servicelocator2.factories.FactoryB1;
 import servicelocator2.factories.FactoryC1;
 import servicelocator2.factories.FactoryD1;
+import servicelocator2.interfaces.Factory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,26 +34,26 @@ class SimpleServiceLocator2Test {
         factoryC1 = new FactoryC1();
         factoryD1 = new FactoryD1();
         ssl = new SimpleServiceLocator();
-        ssl.setService(FactoryA1.class, factoryA1);
-        ssl.setService(F, factoryB1);
-        ssl.setService("C", factoryC1);
-        ssl.setService("D", factoryD1);
-        ssl.setConstant("I", 1);
-        ssl.setConstant("S", "test");
+        ssl.setService(InterfaceA.class,  factoryA1);
+        ssl.setService(InterfaceB.class, factoryB1);
+        ssl.setService(InterfaceC.class, factoryC1);
+        ssl.setService(InterfaceD.class, factoryD1);
+        ssl.setConstant(Integer.class, 1);
+        ssl.setConstant(String.class, "test");
         ssl2 = new SimpleServiceLocator();
-        ssl2.setService("A", factoryA1);
-        ssl2.setService("B", factoryB1);
-        ssl2.setService("C", factoryC1);
-        ssl2.setService("D", factoryD1);
-        ssl2.setConstant("I", 10);
-        ssl2.setConstant("S", "test2");
+        ssl2.setService(InterfaceA.class, factoryA1);
+        ssl2.setService(InterfaceB.class, factoryB1);
+        ssl2.setService(InterfaceC.class, factoryC1);
+        ssl2.setService(InterfaceD.class, factoryD1);
+        ssl2.setConstant(Integer.class, 10);
+        ssl2.setConstant(String.class, "test2");
     }
 
     @Test
     void setService() throws LocatorError {
-        assertEquals(ssl.getObject("B"), factoryB1.create(ssl));
-        assertEquals(ssl.getObject("D"), factoryD1.create(ssl));
-        assertThrows(LocatorError.class, () -> ssl.setService("A", factoryA1));
+        assertEquals(ssl.getObject(InterfaceB.class), factoryB1.create(ssl));
+        assertEquals(ssl.getObject(InterfaceD.class), factoryD1.create(ssl));
+        assertThrows(LocatorError.class, () -> ssl.setService(InterfaceA.class, factoryA1));
     }
 
     @Test
